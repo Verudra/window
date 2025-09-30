1f522abc88da67cd02f2013c2582a8e0f25fa225
Pian jiu <12420306@qq.com>
2025-09-30 23:18:50 +0800

    window code change

--- NewFolder/try.cpp
+++ NewFolder/try.cpp
@@ -1,6 +1,62 @@
-#include<iostream>
-using namespace std;
-int main() {
-	printf("Hello World!");
-	return 0;
+#include <windows.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        TextOutW(hdc, 50, 50, L"Hello, Win32!", 13);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+
+    WNDCLASSEXW wc = {};
+    wc.cbSize = sizeof(WNDCLASSEXW); // 关键：初始化结构体大小
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+    // 检查窗口类注册
+    if (!RegisterClassExW(&wc)) {
+        MessageBoxW(NULL, L"注册窗口类失败", L"错误", MB_ICONERROR);
+        return 0;
+    }
+
+    // 创建窗口
+    HWND hwnd = CreateWindowExW(
+        0,
+        CLASS_NAME,
+        L"Win32 API 窗口示例",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if (hwnd == NULL) {
+        MessageBoxW(NULL, L"创建窗口失败", L"错误", MB_ICONERROR);
+        return 0;
+    }
+
+    ShowWindow(hwnd, SW_SHOWNORMAL); // 强制显示窗口
+    UpdateWindow(hwnd);
+
+    // 消息循环
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return (int)msg.wParam;
 }
